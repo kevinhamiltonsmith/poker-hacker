8b1ccb8ad1555c8fa8206b620ca2058ce8272355
Hack Reactor Students <github@hackreactor.com>
2013-06-04 20:55:18 -0700

    formatting for session time and date

--- public/app/models/Session.js
+++ public/app/models/Session.js
@@ -13,8 +13,13 @@ var Session = Backbone.Model.extend({
       this.set({parenProfit: '$' + netProfit});
       this.set({profitClass: 'session-win'});
     }
+
     var date = this.get('dateStart');
     date = date.substr(0, date.indexOf(','));
     this.set({date: date});
+
+    var time = this.get('sessionLength');
+    time = time.split('').slice(0, -3).join('');
+    this.set({sessionLength: time});
   }
 });
