a675828be0ea5391dcdd9dfaa9c9f7249d7be596
Kevin Smith <kevinhsmitty@gmail.com>
2013-06-05 14:34:17 -0700

    set up comingsoon static html

--- public/web.js
+++ public/web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  app.render("comingsoon.html");
 });
 
 var port = process.env.PORT || 5000;
