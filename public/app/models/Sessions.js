277ff408d5999be3bf2f9463d8286c30dde8620b
Kevin Smith <kevinhsmitty@gmail.com>
2013-06-12 12:24:43 -0600

    calculated overview data for hours, profit, and win rate

--- public/app/models/Sessions.js
+++ public/app/models/Sessions.js
@@ -1,3 +1,25 @@
 var Sessions = Backbone.Collection.extend({
-  model: Session
+  model: Session,
+
+  totalSessions: function() {
+    return this.length;
+  },
+
+  totalWon: function() {
+    return this.reduce(function(memo, value){
+      return memo + value.get('netProfit');
+    }, 0);
+  },
+
+  totalHours: function() {
+    return this.reduce(function(memo, value){
+      var timeParts = value.get('sessionLength').split(/:/);
+      var timePeriodHours = (parseInt(timeParts[0], 10)) + (parseInt(timeParts[1], 10) / 60) + (parseInt(timeParts[2], 10) / 3600);
+      return memo + timePeriodHours;
+    }, 0);
+  },
+
+  winRate: function() {
+    return (this.totalWon() / this.totalHours()).toFixed(2);
+  }
 });
--- public/app/views/OverviewView.js
+++ public/app/views/OverviewView.js
@@ -6,7 +6,13 @@ var OverviewView = Backbone.View.extend({
     '<div class="row">' +
       '<div class="centered six columns">' +
         '<h3>All Results</h3>' +
-
+        '<div class="overview-chart">Profit Chart</div>' +
+        '<table class="sesh-detail">' +
+          '<tr><td>Number of Sessions</td><td class="data-column"><%= numSessions %></td></tr>' +
+          '<tr><td>Total Won</td><td class="data-column">$<%= totalWon %></td></tr>' +
+          '<tr><td>Total Hours</td><td class="data-column"><%= totalHours %></td></tr>' +
+          '<tr><td>Average Win Rate</td><td class="data-column">$<%= winRate %> / hour</td></tr>' +
+        '</table>' +
       '</div>' +
     '</div>'
   ),
@@ -18,7 +24,13 @@ var OverviewView = Backbone.View.extend({
   },
 
   render: function(){
-    console.log(this.collection)
-    return this.$el.html(this.template());
+    var overView = {};
+
+    overView.numSessions = this.collection.totalSessions();
+    overView.totalWon = this.collection.totalWon();
+    overView.totalHours = this.collection.totalHours().toFixed(2);
+    overView.winRate = this.collection.winRate();
+
+    return this.$el.html(this.template(overView));
   }
 });
