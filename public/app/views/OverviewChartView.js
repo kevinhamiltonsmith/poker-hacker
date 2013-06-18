788d632941abe7101b724dc426ef98e370306277
Kevin Smith <kevinhsmitty@gmail.com>
2013-06-17 17:38:47 -0700

    added chart ticks and styling with demo data

--- public/app/views/OverviewChartView.js
+++ public/app/views/OverviewChartView.js
@@ -7,13 +7,13 @@ var OverviewChartView = Backbone.View.extend({
   ),
 
   initialize: function() {
-    var data = [3,7,9,1,4,6,8,2,5];
-    var w = 600;
-    var h = 300;
-    var max = d3.max(data);
+    data = [3,7,9,1,4,6,8,2,5];
+    w = 580;
+    h = 350;
+    max = d3.max(data);
 
-    var x = d3.scale.linear().domain([0, data.length - 1]).range([0, w]);
-    var y = d3.scale.linear().domain([0, max]).range([h, 0]);
+    x = d3.scale.linear().domain([0, data.length - 1]).range([0, w]);
+    y = d3.scale.linear().domain([0, max]).range([h, 0]);
 
     var vis = d3.select(this.el)
       .append('svg:svg')
@@ -28,6 +28,46 @@ var OverviewChartView = Backbone.View.extend({
           return x(i);
         })
         .y(y))
+
+    var ticks = vis.selectAll('.ticky')
+      .data(y.ticks(7))
+      .enter().append('svg:g')
+      .attr('transform', function(d) {
+        return "translate(0, " + (y(d)) + ")";
+      })
+      .attr('class', 'ticky');
+    ticks.append('svg:line')
+      .attr('y1', 0)
+      .attr('y2', 0)
+      .attr('x1', 0)
+      .attr('x2', w);
+    ticks.append('svg:text')
+      .text(function(d) {
+        return d;
+      })
+      .attr('text-anchor', 'end')
+      .attr('dy', 2)
+      .attr('dx', -4);
+
+    ticks = vis.selectAll('.tickx')
+      .data(x.ticks(data.length))
+      .enter().append('svg:g')
+      .attr('transform', function(d, i) {
+        return "translate(" + (x(i)) + ", 0)";
+      })
+      .attr('class', 'tickx');
+    ticks.append('svg:line')
+      .attr('y1', h)
+      .attr('y2', 0)
+      .attr('x1', 0)
+      .attr('x2', 0);
+    ticks.append('svg:text')
+      .text(function(d, i) {
+        return i;
+      })
+      .attr('y', h)
+      .attr('dy', 15)
+      .attr('dx', -2);
   },
 
   render: function() {
