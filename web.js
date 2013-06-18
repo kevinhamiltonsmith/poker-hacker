8c18dd5ee3618fbab40d506c32052f1e036ef8f0
Kevin Smith <kevinhsmitty@gmail.com>
2013-06-18 15:52:37 -0700

    updated express server for heroku

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
- 
-server.listen(5000);
-console.log("listening on 5000")
- 
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
+
+var port = process.env.PORT || 5000; 
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
