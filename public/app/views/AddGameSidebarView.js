d9fc283561a052877a312dbe61a9826766189ead
Kevin Smith <kevinhsmitty@gmail.com>
2013-06-19 11:57:20 -0700

    end session events and data completed WIP: saving all data to Parse

--- public/app/views/AddGameSidebarView.js
+++ public/app/views/AddGameSidebarView.js
@@ -14,9 +14,7 @@ var AddGameSidebarView = Backbone.View.extend({
 
     'click .end-session-button': function(event){
       event.preventDefault();
-      $('.end-session-button').fadeOut('fast');
-      $('.top-in-progress-alert').fadeOut('fast');
-      $('.add-game-sidebar-view form fieldset').removeClass('session-in-progress');
+      this.finalizeSesh();
     },
   },
 
@@ -28,14 +26,12 @@ var AddGameSidebarView = Backbone.View.extend({
       this.startSeshEvents();
     }, this);
 
-    var dt = new Date();
-    this.newSesh.set({dateStartRaw: dt});
-    var startDate = dt.getMonth() + 1 + "/" + dt.getDate() + "/" + dt.getFullYear();
-    var startTime = this.formatAMPM(dt);
-    this.newSesh.set({dateStart: startDate + ', ' + startTime});
+    this.newSesh.on('change:cashedOut', function(){
+      this.endRender();
+      this.endSeshEvents();
+    }, this);
 
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    this.newSesh.set({weekday: days[dt.getDay()] });
+    this.setTime(true);
 
     this.newSesh.set({sessionId: this.collection.models.length + 1});
     this.newSesh.set({totalBuyin: $('.buyin-input').val()});
@@ -43,22 +39,64 @@ var AddGameSidebarView = Backbone.View.extend({
     this.newSesh.set({stakes: $('.stakesSelect').val()});
     this.newSesh.set({game: $('.gamesSelect').val()});
     this.newSesh.set({limitType: $('.limitSelect').val()});
+    
+    this.newSesh.save(null, {
+      success: function(newSesh) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + newSesh.id);
+      },
+      error: function(newSesh, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and description.
+        alert('Failed to create new object, with error code: ' + error.description);
+      }
+    });
   },
 
   finalizeSesh: function() {
-//TODO: update Parse with the finalized session details
+    this.setTime(false);
+
+    this.newSesh.set({cashedOut: $('.cashout-input').val()});
+    var profit = this.newSesh.get('cashedOut') - this.newSesh.get('totalBuyin');
+    this.newSesh.set({netProfit: profit});
   },
 
   startSeshEvents: function() {
-    $('.new-session-button').slideUp('fast');
+    $('.new-session-button, .start-hide').hide();
     $('.end-session-button').fadeIn('slow');
-    $('.top-in-progress-alert').slideDown('slow');
-    $('.new-sesh-cashout').fadeIn();
+    $('.top-in-progress-alert, .new-sesh-cashout, .start-show, .new-sesh-start-time').fadeIn();
     $('.add-game-sidebar-view form fieldset').addClass('session-in-progress');
+  },
 
-    $('.start-hide').hide();
-    $('.start-show').fadeIn();
-    $('.new-sesh-start-time').fadeIn();
+  endSeshEvents: function() {
+    $('.top-in-progress-alert, .end-session-button, .end-hide').fadeOut('fast');
+    $('.add-game-sidebar-view form fieldset').removeClass('session-in-progress').addClass('session-complete');
+  },
+
+  setTime: function(start) {
+    var dt = new Date();
+    var currentDate = dt.getMonth() + 1 + "/" + dt.getDate() + "/" + dt.getFullYear();
+    var currentTime = this.formatAMPM(dt);
+    if (start) {
+      this.newSesh.set({dateStartRaw: dt});
+      this.newSesh.set({dateStart: currentDate + ', ' + currentTime});
+      var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+      this.newSesh.set({weekday: days[dt.getDay()] });
+    } else {
+      this.newSesh.set({dateEndRaw: dt});
+      this.newSesh.set({dateEnd: currentDate + ', ' + currentTime});
+    }
+  },
+
+  formatAMPM: function(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
   },
 
   template: _.template(''+
@@ -69,17 +107,17 @@ var AddGameSidebarView = Backbone.View.extend({
           '<ul>' +
             '<li class="prepend field new-sesh-cashout">' +
               '<div class="row"><label class="default label">Cash Out</label></div>' +
-              '<span class="adjoined">$</span>' +
-              '<input class="wide text input cashout-input" type="text" placeholder="Cash Out" />' +
-            '</li>' +
-            '<li class="field new-sesh-start-time">' +
-              '<div class="row"><label class="default label">Start Date, Time</label></div><h5><%= dateStart %></h5>' +
+              '<span class="adjoined end-hide">$</span>' +
+              '<input class="wide text input cashout-input end-hide" type="text" placeholder="Cash Out" />' +
             '</li>' +
             '<li class="prepend field new-sesh-buyin">' +
               '<div class="row"><label class="default label">Initial Buyin</label></div>' +
               '<span class="adjoined start-hide">$</span>' +
               '<input class="wide text input start-hide buyin-input" type="text" placeholder="Buyin" />' +
               '<h5 class="start-show">$<%= totalBuyin %></h5>' +
+            '</li>' +
+            '<li class="field new-sesh-start-time">' +
+              '<div class="row"><label class="default label">Start Date, Time</label></div><h5><%= dateStart %></h5>' +
             '</li>' +
             '<li class="field new-sesh-location">' +
               '<div class="row"><label class="default label">Location</label></div>' +
@@ -137,19 +175,13 @@ var AddGameSidebarView = Backbone.View.extend({
     '</form>'
   ),
 
-  render: function(){
+  render: function() {
     var allData = $.extend({}, this.model.attributes, this.newSesh.attributes);
     return this.$el.html(this.template(allData));
   },
 
-  formatAMPM: function(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+  endRender: function() {
+    var html = '<h5>$' + this.newSesh.get('cashedOut') + '</h5>';
+    $('.new-sesh-cashout').append(html);
   }
 });
