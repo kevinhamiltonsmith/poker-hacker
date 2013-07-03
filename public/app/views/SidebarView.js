a3d7ee6418eec77740cb109ea2e7dd09286c060d
Kevin Smith <kevinhsmitty@gmail.com>
2013-07-03 15:30:12 -0700

    added sidebar template

--- public/app/views/SidebarView.js
+++ public/app/views/SidebarView.js
@@ -2,6 +2,8 @@ var SidebarView = Backbone.View.extend({
 
   className: 'sidebar',
 
+  template: _.template(''),
+
   loginSidebar: function() {
     this.loginView = new LoginView();
     this.$el.prepend(this.loginView.render().el);
